08d0ae357e92e9f5a08372527e728bc772981064
Luma Zamarreño <zamarrenolm@aia.es>
2016-09-19 18:28:18 +0200

    renamed/moved files

---
+++ commons/src/main/java/org/power_systems_modelica/psm/commons/Configuration.java
@@ -0,0 +1,24 @@
+package org.power_systems_modelica.psm.commons;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Configuration
+{
+	public String getParameter(String name)
+	{
+		return parameters.get(name);
+	}
+
+	public boolean getBoolean(String name)
+	{
+		return Boolean.valueOf(getParameter(name));
+	}
+
+	public void setParameter(String name, String value)
+	{
+		parameters.put(name, value);
+	}
+
+	private final Map<String, String> parameters = new HashMap<>();
+}

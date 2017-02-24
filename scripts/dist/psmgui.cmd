2426b96d5efea2e62d7024fc89c411a48a80b4b8
Luma Zamarreño <zamarrenolm@aia.es>
2017-02-24 17:08:19 +0100

    First version of startup script for windows

---
+++ scripts/dist/psmgui.cmd
@@ -0,0 +1,35 @@
+ECHO ""
+SET PSM_DATA=./data
+SET PATH=%PATH%:.\helmflow\windows
+
+:Check1
+IF [%OPENMODELICAHOME%] == [] GOTO MissingOpenModelicaHome
+:Check2
+IF [%DYMOLAHOME%] == [] GOTO MissingDymola
+:Check3
+IF [%OPENMODELICAHOME%%DYMOLAHOME] == [] GOTO NoDynEngine
+GOTO Start
+
+:MissingOpenModelicaHome
+ECHO "Missing variable OPENMODELICAHOME"
+GOTO Check2
+
+:MissingDymola
+ECHO "Missing variable DYMOLAHOME"
+GOTO Check3
+
+:NoDynEngine
+ECHO "Neither OPENMODELICAHOME nor DYMOLAHOME are defined"
+EXIT
+
+:Start
+SET PSM_OPTIONS="-Ditesla.config.dir=%PSM_DATA%/test/cfg
+SET PSM_HELMFLOW_JAR=./lib/helmflow_core-2.2.5.201702061800.0.jar
+SET PSM_CLASSPATH="-cp ./lib/gui-0.1-SNAPSHOT-jar-with-dependencies.jar;./lib/psm-services.jar;./lib/modelica_java.jar;%PSM_HELMFLOW_JAR%;%PSM_DATA%/test/cfg
+SET PSM_MAIN=org.power_systems_modelica.psm.gui.MainApp
+ECHO "Java options    : %PSM_OPTIONS%"
+ECHO "Java classpath  : %PSM_CLASSPATH%"
+ECHO "Java main class : %PSM_MAIN%"
+ECHO ""
+
+java %PSM_CLASSPATH% %PSM_OPTIONS% %PSM_MAIN% 1> psm.log 2>&1
